20e617b13597a4cfe509b81241f6c3f3ec81716b
RichardC++ <44072670+GLoryforRichard@users.noreply.github.com>
2026-02-03 20:45:49 -0500

    Initial Commit

---
+++ tongzinumber-demo/tongzinumber_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tongzinumber_demoApp.swift
+//  tongzinumber-demo
+//
+//  Created by Richard Liu on 2026-02-03.
+//
+
+import SwiftUI
+
+@main
+struct tongzinumber_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tongzinumber-demoTests/tongzinumber_demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  tongzinumber_demoTests.swift
+//  tongzinumber-demoTests
+//
+//  Created by Richard Liu on 2026-02-03.
+//
+
+import Testing
+@testable import tongzinumber_demo
+
+struct tongzinumber_demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
